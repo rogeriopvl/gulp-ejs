0a2c7496cdb1c65b84f889cfdc0dd5aa5154afb2
Stanislav Simovski <megakoresh@gmail.com>
2017-06-16 19:04:32 +0300

    Remove debug code, please stop the linting

--- index.js
+++ index.js
@@ -31,7 +31,6 @@ module.exports = function (data, options, settings) {
       if (options.client === true) {
         var name = file.basename.split(/\./)[0].toLowerCase() + (settings.suffix !== undefined ? settings.suffix : '')
         var template = ejs.compile(file.contents.toString(), options)
-        var templateFunctionBody = template.toString().match(/function[^{]+\{([\s\S]*)\}$/)[1]+'\n}'
         options.filename = file.basename
         file.contents = new Buffer(
           'if(window.templates === undefined) window.templates = {}; ' +
